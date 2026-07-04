3c8030f72f055c93cb14590e340856ea11cfbbba
Rafifhibatullahsudo <rafifbehehe@gmail.com>
2026-07-04 23:07:38 +0700

    Update semua file

--- login.php
+++ login.php
@@ -5,19 +5,20 @@ session_start();
 include 'config/app.php';
 global $db;
 
-//check apakah tombol login ditekan
+// check apakah tmbol login ditekan
 if (isset($_POST['login'])) {
     // ambil input username dan password
-    $username =  mysqli_real_escape_string($db, $_POST['username']);
-    $password =  mysqli_real_escape_string($db, $_POST['password']);
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
 
-    //checkIntelephense: Clear Cache
+    // check username
     $result = mysqli_query($db, "SELECT * FROM akun WHERE username = '$username'");
 
-    // check password
+    // jika ada usernya
     if (mysqli_num_rows($result) == 1) {
-        //check passwordnya
+        // check passwordnya
         $hasil = mysqli_fetch_assoc($result);
+
         if (password_verify($password, $hasil['password'])) {
             // set session
             $_SESSION['login']      = true;
@@ -27,87 +28,105 @@ if (isset($_POST['login'])) {
             $_SESSION['email']      = $hasil['email'];
             $_SESSION['level']      = $hasil['level'];
 
+            // jika login benar arahkan ke file index.php
             header("Location: index.php");
             exit;
         }
     }
-    //jika tidak ada usernya/login salah
+    // jika tidak ada usernya/login salah
     $error = true;
 }
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <title>admin login</title>
+    <title>Admin Login</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
-
-
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets-template/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="assets-template/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="assets-template/dist/css/adminlte.min.css">
     <!-- Favicons -->
-    <link rel="icon" href="assets/img/bootstrap-logo.svg">
-    <meta name="theme-color" content="#7952b3">
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
-
-    <!-- Custom styles for this template -->
-    <link href="assets/css/signin.css" rel="stylesheet">
+    <link rel="icon" href="assets-template/img/bootstrap-logo.svg">
 </head>
 
-<body class="text-center">
-
-    <main class="form-signin">
-        <form action="" method="POST">
-            <img class="mb-4" src="assets/img/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h1 class="h3 mb-3 fw-normal">admin login</h1>
-
-            <?php if (isset($error)) : ?>
-                <div class="alert alert-danger text-center">
-                    <b>Username/password salah</b>
-                </div>
-            <?php endif; ?>
-
-            <div class="form-floating">
-                <input type="text" name="username" class="form-control" id="floatingInput" placeholder="username" required>
-                <label for="floatingInput">username</label>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <div class="text-center">
+                <img class="mb-4" src="assets-template/img/bootstrap-logo.svg" alt="" width="72" height="57">
+                <a href="#"><b>Admin</b>LTE</a>
             </div>
-            <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password...." required>
-                <label for="floatingPassword">Password</label>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Masukan username dan password</p>
+
+                <?php if (isset($error)) : ?>
+                    <div class="alert alert-danger text-center">
+                        <b>Username/Password SALAH</b>
+                    </div>
+                <?php endif; ?>
+
+                <form action="" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="username" placeholder="Username..." required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password..." required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-8">
+                        </div>
+
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" name="login" class="btn btn-primary btn-block">Masuk</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <hr>
+                <p class="mb-1 text-center">
+                    <span class="mt-5 mb-3 text-muted">Developer &copy;
+                        <a href="https://mubatekno.com">Muba Teknologi</a> <?= date('Y') ?>
+                    </span>
+                </p>
             </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
 
-            <button class="w-100 btn btn-lg btn-primary" type="submit" name="login">login</button>
-            <p class="mt-5 mb-3 text-muted"> Copyright&copy; RAFIF <?= date('Y') ?></p>
-        </form>
-    </main>
-
-
+    <!-- jQuery -->
+    <script src="assets-template/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="assets-template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="assets-template/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
